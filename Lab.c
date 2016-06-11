b9153c862662cb8304c8b3ed58fefaaf24b1c46b
ptokmakov <ptokmakov501@gmail.com>
2016-06-11 07:59:01 +0600

    added coefficients of the variables

--- Lab.c
+++ Lab.c
@@ -6,5 +6,13 @@ int main()
 	float a,b,c,D;
 	float x1,x2,x;
 	
+	printf ("Input A,B,C: \n");	
+	printf("A=");
+	scanf("%f", &a);
+	printf("B=");
+	scanf("%f", &b);
+	printf("C=");
+	scanf("%f", &c);
+	
 	return 0;
 }
